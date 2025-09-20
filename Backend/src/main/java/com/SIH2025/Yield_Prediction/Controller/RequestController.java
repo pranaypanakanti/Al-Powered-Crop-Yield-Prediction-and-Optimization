54b9aefe776a4084f7cffb4ceeee62e85fe5ac10
panakantipranay <panakantipranay@gmail.com>
2025-09-20 19:45:03 +0530

    Create Controller, Service and Repo layers.

---
+++ Backend/src/main/java/com/SIH2025/Yield_Prediction/Controller/RequestController.java
@@ -0,0 +1,12 @@
+package com.SIH2025.Yield_Prediction.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class RequestController {
+
+}
---
+++ Backend/src/main/java/com/SIH2025/Yield_Prediction/Repository/RepoManager.java
@@ -0,0 +1,8 @@
+package com.SIH2025.Yield_Prediction.Repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepoManager {
+
+}
