c954c8fc748f7877318d2721dc2b245d1738c5e9
panakantipranay <panakantipranay@gmail.com>
2025-09-20 12:29:50 +0530

    Add backend configuration setup

---
+++ Backend/src/main/java/com/SIH2025/Yield_Prediction/YieldPredictionApplication.java
@@ -0,0 +1,13 @@
+package com.SIH2025.Yield_Prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YieldPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YieldPredictionApplication.class, args);
+	}
+
+}
